f628b1e030d51a00ee24984a98ef7e7227db8ea7
Abeermohsien <140171839+Abeermohsien@users.noreply.github.com>
2024-09-14 07:11:14 +0300

    Update App.js

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Auth from './components/Auth';
+import Chat from './components/Chat';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState('');
+    const [user, setUser] = useState(null);
+
+    return (
+        <div className="App">
+            {token ? <Chat token={token} user={user} /> : <Auth setToken={setToken} setUser={setUser} />}
+        </div>
+    );
 }
 
 export default App;
